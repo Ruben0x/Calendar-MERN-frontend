8b214bd2853368e6954a906f9af995d26c0f7a22
ruben <r.alejandro.gn@gmail.com>
2024-07-12 13:19:47 -0400

    first commit

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { useAuthStore } from '../../hooks';
+
+export const Navbar = () => {
+  const { startLogout, user } = useAuthStore();
+  return (
+    <div className='navbar navbar-dark bg-dark mb-4 px-4'>
+      <span className='navbar-brand'>
+        <i className='fas fa-calendar' aria-hidden='true'></i>
+        &nbsp; {user.name}
+      </span>
+      <button className='btn btn-outline-danger' onClick={startLogout}>
+        <i className='fas fa-sign-out' aria-hidden='true'></i>&nbsp;
+        <span>Salir</span>
+      </button>
+    </div>
+  );
+};
---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import {
+  CalendarEvent,
+  CalendarModal,
+  FabAddNew,
+  FabDelete,
+  Navbar,
+} from '../';
+import { getMessagesES, localizer } from '../../helpers';
+import { useCalendarStore, useUiStore } from '../../hooks';
+
+export const CalendarPage = () => {
+  const { openDateModal } = useUiStore();
+  const { events, setActiveEvent } = useCalendarStore();
+  const [lastView, setLastView] = useState(
+    localStorage.getItem('lastView') || 'week'
+  );
+
+  const eventStyleGetter = () => {
+    const style = {
+      backgroundColor: '#347cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    };
+
+    return {
+      style,
+    };
+  };
+  const onDoubleClick = (event) => {
+    openDateModal();
+  };
+  const onSelect = (event) => {
+    setActiveEvent(event);
+  };
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+  };
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        culture='es-Es'
+        defaultView={lastView}
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{ event: CalendarEvent }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+      />
+      <CalendarModal />
+      <FabAddNew />
+      <FabDelete />
+    </>
+  );
+};
